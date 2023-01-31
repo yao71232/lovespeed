268847aeaf2a1d2fd77929156874d49deb979c99
yao71232 <77203858+yao71232@users.noreply.github.com>
2023-01-31 22:20:06 +0800

    Update lovespeed.sh

--- lovespeed.sh
+++ lovespeed.sh
@@ -5,4 +5,3 @@ rm -rf "/tmp/speed"
 "/root/speedtest-cli/speedtest"  >  "/tmp/speed"
 ls1=`cat /tmp/speed | grep Upload | awk '{gsub(/^\s+|\s+$/, "");print}'`
 ls2=`cat /tmp/speed | grep Download | awk '{gsub(/^\s+|\s+$/, "");print}'`
-lsn=hostname
