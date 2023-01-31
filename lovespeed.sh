0e9253f14da2188f5b8c764708a70d2f8b1c7bdd
yao71232 <77203858+yao71232@users.noreply.github.com>
2023-01-31 22:17:26 +0800

    Update lovespeed.sh

--- lovespeed.sh
+++ lovespeed.sh
@@ -6,9 +6,3 @@ rm -rf "/tmp/speed"
 ls1=`cat /tmp/speed | grep Upload | awk '{gsub(/^\s+|\s+$/, "");print}'`
 ls2=`cat /tmp/speed | grep Download | awk '{gsub(/^\s+|\s+$/, "");print}'`
 lsn=hostname
-tgid=
-tgbotapi=
-curl --data chat_id="$tgid" --data-urlencode "text=§$lsn§
-
-$ls1
-$ls2" "https://api.telegram.org/bot$tgbotapi/sendMessage?parse_mode=HTML"
