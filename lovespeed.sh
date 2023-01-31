899339e91a69bd178644d917487a5cd449fae95e
yao71232 <77203858+yao71232@users.noreply.github.com>
2023-01-31 22:18:51 +0800

    Update lovespeed.sh

--- lovespeed.sh
+++ lovespeed.sh
@@ -2,7 +2,7 @@
 . /etc/profile
 . ~/.bash_profile 
 rm -rf "/tmp/speed"
-"/root/speedtest"  >  "/tmp/speed"
+"/root/speedtest-cli/speedtest"  >  "/tmp/speed"
 ls1=`cat /tmp/speed | grep Upload | awk '{gsub(/^\s+|\s+$/, "");print}'`
 ls2=`cat /tmp/speed | grep Download | awk '{gsub(/^\s+|\s+$/, "");print}'`
 lsn=hostname
